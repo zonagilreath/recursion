81920ec904f16d7c51160157ad852fea9785696e
zonagilreath <bryson.gilreath@gmail.com>
2019-05-10 13:26:39 -0500

    implemented getElementsByClassName, passing tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  if (this === window) return getElementsByClassName.call(document.body, className);
+  let elementsWithClass = [];
+	if (this.classList){
+		if (this.classList.contains(className)) {
+			elementsWithClass.push(this);
+		}
+	}
+	if (this.hasChildNodes()){
+	  let nodes = this.childNodes;
+	  for (let node of nodes){
+	  	elementsWithClass = elementsWithClass.concat(getElementsByClassName.call(node, className));
+	  }
+	}
+  return elementsWithClass;
 };

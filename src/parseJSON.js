08834d9621597fb4f2693ae70695851a0f021a97
zonagilreath <bryson.gilreath@gmail.com>
2019-05-14 15:26:44 -0500

    chunker helper function implemented

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -12,30 +12,8 @@ var parseJSON = function(json) {
   if (json === 'true') return true;
   if (json === 'false') return false;
   if (Number(json) == json) return Number(json);
-  if (json[0] === '['){
-  	let parsed = [];
-  	let unwrapped = json.slice(1, json.length - 1);
-  	if (unwrapped.length === 0) return parsed;
-  	for (let substring of unwrapped.split(',')){
-  		parsed.push(parseJSON(substring));
-  	}
-  	return parsed;
-  }
-  if (json[0] === '{') {
-  	let parsed = {};
-  	let unwrapped = json.slice(1, json.length - 1);
-  	if (unwrapped.length === 0) return parsed;
-  	console.log('unwrapped: ', unwrapped);
-  	for (let substring of unwrapped.split(',')){
-  		console.log('substring: ', substring);
-  		let pair = substring.split(':');
-  		console.log('pair: ', pair);
-  		let key = parseJSON(pair[0]);
-  		let value = parseJSON(pair[1]);
-  		parsed[key] = value;
-  	}
-  	return parsed;
-  }
+  let chunks = chunker(json);
+
 };
 
 function chunker(string){
@@ -61,8 +39,16 @@ function chunker(string){
 			if (characters[0] === '"'){
 				stringOpen = true;
 				chunks.push(characters.shift());
-			}else if (!voids.contains(characters[0])){
-				chunks.push(characters.shift());
+			}else if (!voids.includes(characters[0])){
+				if (delimiters.includes(characters[0])){
+					chunks.push(characters.shift());
+				}else{
+					let nonString = '';
+					while (!delimiters.includes(characters[0])){
+						nonString += characters.shift();
+					}
+					chunks.push(nonString);
+				}
 			}else {
 				characters.shift();
 			}
